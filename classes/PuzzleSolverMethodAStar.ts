4f985318c6e987eeef22792e76a964f3174ff85c
PavelSatyrenko <zevsp09@gmail.com>
2024-05-15 16:48:58 +0300

    completed a star method

--- classes/PuzzleSolverMethodAStar.ts
+++ classes/PuzzleSolverMethodAStar.ts
@@ -1,53 +1,32 @@
 import { PuzzleSolver } from './PuzzleSolver';
+import { PriorityQueue } from './PriorityQueue';
 
 export class PuzzleSolverMethodAStar extends PuzzleSolver {
-    solve(initialState: number[][]): State | null {
-        const initialStateObj: State = {
-            tiles: initialState,
-            emptyTilePos: { row: 0, col: 0 },
-            cost: 0,
-            heuristic: 0,
-            totalCost: 0
-        };
-        initialStateObj.emptyTilePos = this.findEmptyTilePosition(initialStateObj);
-
+    solve(initialState: State): State | null {
         const openSet = new PriorityQueue<State>();
         const closedSet = new Set<string>();
-        openSet.enqueue(initialStateObj, initialStateObj.totalCost);
+
+        openSet.enqueue(initialState, 0);
 
         while (!openSet.isEmpty()) {
             const currentState = openSet.dequeue()!;
-            if (this.isGoalState(currentState)) {
+
+            if (this.stateToString(currentState) == "123456780") {
                 return currentState;
             }
 
             closedSet.add(this.stateToString(currentState));
 
-            const successorStates = this.getSuccessorStates(currentState);
-            for (const successorState of successorStates) {
-                if (closedSet.has(this.stateToString(successorState))) {
+            const availableStates = this.getAvailableStates(currentState);
+            for (const state of availableStates) {
+                if (closedSet.has(this.stateToString(state))) {
                     continue;
                 }
-                openSet.enqueue(successorState, successorState.totalCost);
+
+                openSet.enqueue(state, state.totalCost);
             }
         }
 
-        return null; // No solution found
-    }
-
-    findEmptyTilePosition(state: State): { row: number, col: number } {
-        for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) {
-                if (state.tiles[i][j] === 0) {
-                    return { row: i, col: j };
-                }
-            }
-        }
-        throw new Error("Invalid state: Empty tile not found.");
-    }
-
-    isGoalState(state: State): boolean {
-        const goalState: string = "123456780";
-        return this.stateToString(state) === this.stateToString({ tiles: this.goalState, emptyTilePos: { row: 0, col: 0 }, cost: 0, heuristic: 0, totalCost: 0 });
+        return null;
     }
 }
